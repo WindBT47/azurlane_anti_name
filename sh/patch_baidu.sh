2ca9691b7074fbcc0dc687943d2d09cf7e9b2f1c
WindBT47 <45734041+WindBT47@users.noreply.github.com>
2024-05-18 00:17:58 +0800

    Update patch_baidu.sh
    Change to lenovo

--- sh/patch_baidu.sh
+++ sh/patch_baidu.sh
@@ -4,7 +4,7 @@
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
     #这个链接是百度 https://mobile.baidu.com/item?docid=5008915318&f0=search_searchContent%400_appBaseNormal%400&advitem=source%2Bnatural%40pos%2B1%40bid%2B0%40ctr%2B0.000000%40trans%2B%40keyword%2B%E7%A2%A7%E8%93%9D%E8%88%AA%E7%BA%BF%40app_rcv%2B
-    url="https://112-29-199-43-yf11103f.nodeedge.cn:56987/fsd0c717e3.a.bdydns.com/0/service/cloudapk_sign_online/69000/69265/69265_20240428162519_60031_signed.apk?prodid=2&bcecdn_trace_info=C24sUVlRAhsuDh03YmpjTVI8ZAMVCk1XPxUaECc7JQ9SMR88I1dUF1YINgQRITQ8L1A%3D&f_ver=v1&max_age=7776000&r=GDgySURJFxYqHRMhM3YjBV4oM1ZcBhZaNhlbPDIqNwgTKWlaWxxNXTgEHxAkMSYPLyMoVV4dXRZvTUR%2FZ3d3WEJ6cxYBSgoPbCtGf2VscVVCdHcPBUYJAAZCRH9kaR4SGSsoXFNdWUkySwQ9ODwoBU1%2BHzwjV1QXVgg2BBEhNDwvUA%3D%3D&MqD7sp=l56Ml8Kdm4abkYygmszHyZydm53KyZ2bmc6ZyMyZzM3IzczPz5vOnM3JmZmcyJzLypzNm9mGmZuMj4vCzsjOysvPy8jMzc3Gx9mMnIqWm8KFk5q7x42tm7OQhZump76auKqInNmGmY-NlsLOz8_ZnJOMi5DCzNmLlpKakIqLws7IzsjGxsnIzM3ZnJeanJTCzM3Hy8fMyM7Iy9mNmo6Mj5vCzM_P2YaZkI-LwsrNzg~~&UYs5cp=hpmXkIyLws7NyNHP0c_RztmGmY2ajpabws3Onp2dzZvMxsvKnsjKms-eysidzc6enc_PyJvIxsbP2YaZnp2Mws_ZhpmMlJaPws3Zhpmcm5GQj4vCz9mGmYuWkprCzsjOysvPy8jMzczPyQ~~"
+    url="https://apks7.lenovomm.cn/dlserver/fileman/ali/app/app-apkg-lestore/1345-2024-04-28014912-1714283352316.apk?sign=8639805f47b3c2765af46d357eb35132&t=664a25fc&order=0&uuid=e34c6ebca082471586b49bd904ccae83&cMD5=false&sorder=0&group=&ts=1715962620290&cpn=com.lenovo.appstore.3g&cid=17071"
     # 使用curl命令下载apk文件
     axel -n 16 -k -o blhx.apk $url
     fi
