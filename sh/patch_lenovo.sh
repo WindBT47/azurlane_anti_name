6b2bfef3b31dbef0a7be49ab5e2368c2c6a216ec
WindBT47 <45734041+WindBT47@users.noreply.github.com>
2026-06-02 17:31:46 +0800

    Update download URL for Azur Lane APK

--- sh/patch_lenovo.sh
+++ sh/patch_lenovo.sh
@@ -4,7 +4,7 @@
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
     #这个链接是百度 https://mobile.baidu.com/item?docid=5008915318&f0=search_searchContent%400_appBaseNormal%400&advitem=source%2Bnatural%40pos%2B1%40bid%2B0%40ctr%2B0.000000%40trans%2B%40keyword%2B%E7%A2%A7%E8%93%9D%E8%88%AA%E7%BA%BF%40app_rcv%2B
-    url="https://apks7.lenovomm.cn/dlserver/fileman/ali/app/app-apkg-lestore/1277-2025-08-20073147-1755689507874.apk?sign=85abb939b39ece620da9b7ba41cd6ddd&t=68b32adf&order=0&uuid=913dd344fa024471b658e82d2a579c2d&cMD5=false&sorder=0&group=&ts=1756399583888&cpn=-1&cid=17071&__bc=1&__cid=17071&__ip=171.222.188.87&__ept=1&dck=1&dp=0"
+    url="https://apks7.lenovomm.cn/dlserver/fileman/ali/app/app-apkg-lestore/7793-2026-05-20114909-1779248949627.apk?sign=6ace8a8c1e418c6305bbb326e84bc71a&t=6a2144af&order=0&uuid=e1e2c1b44b0b4b12b07a328a46cae4ba&cMD5=false&sorder=0&group=&ts=1780392367278&cpn=-1&cid=17071&__bc=1&__cid=17071&__ip=171.213.184.126&__ept=1&dck=1&dp=0"
     # 使用curl命令下载apk文件
     axel -n 16 -k -o blhx.apk $url
     fi
